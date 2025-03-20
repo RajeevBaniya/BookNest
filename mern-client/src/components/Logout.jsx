0d6b7468826a801eb57dab4316d58fffa459f1d0
Rajeev Baniya <baniyarajeev811@gmail.com>
2025-03-20 19:21:29 +0530

    Update Logout.jsx

--- mern-client/src/components/Logout.jsx
+++ mern-client/src/components/Logout.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { AuthContext } from '../Context/AuthProvider'
 import { useLocation, useNavigate } from 'react-router-dom';
 
@@ -8,21 +8,24 @@ const Logout = () => {
     const navigate = useNavigate();
 
     const from = location.state?.from?.pathname || "/";
-    const handleLogout = () => {
+    
+    useEffect(() => {
+        // Automatically log out when the component mounts
         logOut().then(() => {
             // Sign-out successful.
-            alert('Sign-out successful!');
             navigate(from, {replace: true})
           }).catch((error) => {
             // An error happened.
-          });          
-    }
-  return (
-    <div className='h-screen bg-gradient-to-r from-teal-200 to-cyan-400 flex items-center justify-center'>
-      <p className='text-black underline text-3xl mb-14 font-semibold items-center'>Are you sure ? You will no longer be logged in</p>
-        <button className='bg-red-600 hover:bg-red-700 px-8 py-2 text-white rounded absolute mt-9' onClick={handleLogout}>Logout</button>
-    </div>
-  )
+            console.error("Logout error:", error);
+            navigate("/");
+          });
+    }, []);
+    
+    return (
+        <div className='h-screen bg-gradient-to-r from-teal-200 to-cyan-400 flex items-center justify-center'>
+            <p className='text-black text-3xl mb-14 font-semibold items-center'>Logging out...</p>
+        </div>
+    )
 }
 
 export default Logout
