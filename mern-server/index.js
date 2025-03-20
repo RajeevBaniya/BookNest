843c919c73132fd7ccec5847e9d431765a3c8c95
Rajeev Baniya <baniyarajeev811@gmail.com>
2025-03-20 19:23:38 +0530

    Update index.js

--- mern-server/index.js
+++ mern-server/index.js
@@ -3,19 +3,15 @@ const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
 
-
 //middleware
 app.use(cors());
 app.use(express.json());
-
-
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 // mongodb configuration
-
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://book-store:0IJjXLRbMd0k6b2J@cluster0.3firnrp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
@@ -30,31 +26,44 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
-    // creating a collection of documents
     const bookCollections = client.db("BookStore").collection("books");
 
-    // inserting a book to th database using post method
-
+    // inserting a book to the database using post method
     app.post("/upload-book", async(req, res) => {
         const data = req.body;
         const result = await bookCollections.insertOne(data);
         res.send(result);
     })
 
-    // getting all books from the database
-    // app.get("/all-books", async(req, res) => {
-    //     const books = bookCollections.find();
-    //     const result = await books.toArray();
-    //     res.send(result);
-    // })
+    // getting all books from the database with optional search query
+    app.get("/all-books", async(req, res) => {
+        let query = {};
+        
+        // Handle category filter
+        if(req.query?.category) {
+            query.category = req.query.category;
+        }
+        
+        // Handle search query
+        if(req.query?.search) {
+            const searchRegex = new RegExp(req.query.search, 'i');
+            query = {
+                $or: [
+                    { bookTitle: searchRegex },
+                    { authorName: searchRegex },
+                    { category: searchRegex }
+                ]
+            };
+        }
+        
+        const result = await bookCollections.find(query).toArray();
+        res.send(result);
+    })
 
     //updating a book data: patching or updating methods
     app.patch("/book/:id", async(req, res) => {
         const id = req.params.id;
-        // console.log(id);
         const updateBookData = req.body;
         const filter = {_id: new ObjectId(id)};
         const options = { upsert: true };
@@ -65,8 +74,7 @@ async function run() {
             }
         }
 
-        //updating
-        const result = await bookCollections.updateOne(filter, updateDoc, options );
+        const result = await bookCollections.updateOne(filter, updateDoc, options);
         res.send(result);
     })
 
@@ -74,39 +82,25 @@ async function run() {
     app.delete("/book/:id", async(req, res) => {
         const id = req.params.id;
         const filter = {_id: new ObjectId(id)};
-        const result = await bookCollections.deleteOne()
-        res.send(result);
-    })
-
-    //finding by category
-    app.get("/all-books", async(req, res) => {
-        let query = {};
-        if(req.query?.category){
-            query = {category: req.query.category}
-        }
-        const result = await bookCollections.find(query).toArray();
+        const result = await bookCollections.deleteOne(filter);
         res.send(result);
     })
 
     // to get single book data
     app.get("/book/:id", async(req, res) =>{
       const id = req.params.id;
-      const filter = { _id:new ObjectId(id)};
+      const filter = { _id: new ObjectId(id)};
       const result = await bookCollections.findOne(filter);
       res.send(result);
     })
 
-
-    // Sending a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
